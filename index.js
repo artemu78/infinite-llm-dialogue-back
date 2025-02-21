53f32c3b2c24d9de001921a39dc359478a5c99f7
ArtemReva <artemreva79@gmail.com>
2025-02-21 19:57:18 +0300

    Add chat message storage functionality and update package versions

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const AWS = require("aws-sdk");
 const axios = require("axios");
+const { v4: uuidv4 } = require("uuid"); // Add this line to import the uuid library
 
 const VITE_GNEWS_API_KEY = process.env.VITE_GNEWS_API_KEY;
 const NEWS_TABLE_NAME = "InfiniteChat_NewsAPI_Cache";
+const CHAT_TABLE_NAME = "InfiniteChat_ChatLog";
+
 const MODEL_NAME = process.env.GOOGLE_MODEL_NAME;
 const API_KEY = process.env.GOOGLE_API_KEY;
 const NewsURL = `https://gnews.io/api/v4/search?q=artificial intelligence&lang=en&max=5&apikey=${VITE_GNEWS_API_KEY}`;
@@ -79,7 +82,7 @@ async function News(debug) {
       log(debug, "News data found in cache:", data.Item);
       return {
         statusCode: 200,
-        body: JSON.stringify(data.Item),
+        body: JSON.stringify(data.Item?.news),
       };
     } else {
       log(debug, `News data not found in cache. Fetching from API ${NewsURL}`);
@@ -100,7 +103,7 @@ async function News(debug) {
       log(debug, "Stored news data in cache:", putParams.Item);
       return {
         statusCode: 200,
-        body: JSON.stringify(putParams.Item),
+        body: JSON.stringify(newsData),
       };
     }
   } catch (error) {
@@ -109,6 +112,26 @@ async function News(debug) {
       statusCode: 500,
       body: JSON.stringify({ error: "Internal Server Error" }),
     };
+  }
+}
+
+async function storeChatMessage(message, sender, debug) {
+  const params = {
+    TableName: CHAT_TABLE_NAME,
+    Item: {
+      id: uuidv4(), // Generate a unique ID
+      datetime: Date.now(), // Get the current timestamp
+      message: message,
+      sender: sender,
+    },
+  };
+
+  try {
+    await dynamoDB.put(params).promise();
+    log(debug, "Stored chat message in DynamoDB:", params.Item);
+  } catch (error) {
+    console.error("Error storing chat message:", error);
+    throw error;
   }
 }
 
@@ -133,7 +156,7 @@ exports.handler = async (event) => {
         body: JSON.stringify({ error: "Missing userInput in request body" }),
       };
     }
-
+    await storeChatMessage(userInput, "user", debug);
     const chosenPersonalities = [];
     const numPersonalities = Math.random() < 0.5 ? 1 : 3; // Randomly choose 1 or 3 personalities
 
@@ -153,6 +176,7 @@ exports.handler = async (event) => {
       chosenPersonalities.map(async (personalityKey) => {
         const personality = personalities[personalityKey];
         const response = await generateResponse(userInput, personality, debug);
+        await storeChatMessage(response, personalityKey, debug); // Store each generated response
         return {
           personality: personalityKey,
           response: response,
